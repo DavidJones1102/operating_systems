31c4fd3f135687903d7244ea4604ec83316d0384
Szymon Głomski <szymon.glomski@gmail.com>
2023-03-21 14:34:02 +0100

    zad1 new solution

--- cw03/zad1/zad1.c
+++ cw03/zad1/zad1.c
@@ -7,30 +7,30 @@
 #include<sys/wait.h>
 #include"zad1.h"
 
-void make_process(int i, int max){
-
-    pid_t child_pid = fork();
-    if(child_pid == 0){
-        printf("PPID: %d, PID: %d\n",(int)getppid(),(int)getpid());
-    }
-    else if(i+1<max){
-        make_process(i+1, max);
-    }
-    else{
-        for(int i=0; i<max; i++) wait(NULL);
-        printf("Number of processes: %d\n",max);
-    }
-
-}
-
 int main( int argc, char*argv[]){
     if(argc != 2 || is_number(argv[1]) == 0){
         fprintf(stderr, "Wrong arguments\n");
         return -1;
     }
     int n = atoi(argv[1]);
-    make_process(0,n);
 
+    pid_t* arr = malloc(sizeof(pid_t)*n);
+    pid_t pid = getpid();
+    for(int i = 0; i<n;i++){
+        if(pid == getpid()){
+            arr[i] = fork();
+        }
+
+        if(pid != getpid()){
+            printf("PPID: %d, PID: %d\n",(int)getppid(),(int)getpid());
+            break;
+        }
+    }
+
+    if(pid == getpid()){
+        for(int i=0; i<n; i++) waitpid(arr[i],NULL,0);
+        printf("Number of processes: %d\n",n);
+    }
     return 0;
 }
 
